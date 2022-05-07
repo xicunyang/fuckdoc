14245468785177ae0e0444892dc8db8631aa2f54
yangxicun <yangxicun@bytedance.com>
2022-05-07 09:52:22 +0800

    feat: 添加vscode插件

--- packages/fuckdoc-vscode-extension/src/extension.ts
+++ packages/fuckdoc-vscode-extension/src/extension.ts
@@ -1,55 +1,125 @@
-import * as vscode from 'vscode'
-const fs = require('fs')
-const sharp = require('sharp')
+import * as vscode from 'vscode';
+const fs = require('fs');
+const shorthash = require('shorthash');
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log('插件启动成功')
+let globalSelectedFilePath;
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand('fuckdoc.addPic', async (uri: any) => {
-      const selectedFilePath = uri.path as string
-      const prefixPath = selectedFilePath.substring(
-        0,
-        selectedFilePath.lastIndexOf('/')
-      )
-      const panel = vscode.window.createWebviewPanel(
-        'addPic',
-        '添加图片',
-        vscode.ViewColumn.One,
-        {
-          enableScripts: true,
-          localResourceRoots: [
-            vscode.Uri.joinPath(context.extensionUri, 'media')
-          ]
+function getNonce() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+class WebviewPanel {
+  public static currentPanel;
+  private selectedFilePath;
+  private extensionUri;
+  private _disposables: vscode.Disposable[] = [];
+  private _isDiff;
+
+  constructor(selectedFilePath, extensionUri, isDiff) {
+    this.selectedFilePath = selectedFilePath;
+    this.extensionUri = extensionUri;
+    this._isDiff = isDiff;
+
+    // 如果panel不存在，就打开一个新的
+    if (!WebviewPanel.currentPanel) {
+      WebviewPanel.currentPanel = this._createPanel();
+      this._initEvent();
+    } else if (isDiff) {
+      this._dispose();
+      WebviewPanel.currentPanel = this._createPanel();
+      this._initEvent();
+    } else {
+      // 否则打开原来的
+      WebviewPanel.currentPanel.reveal();
+    }
+    this._update();
+  }
+
+  _initEvent() {
+    WebviewPanel.currentPanel.onDidDispose(() => this._dispose(), null, this._disposables);
+
+    WebviewPanel.currentPanel.webview.onDidReceiveMessage(
+      message => {
+        if (message.type === 'CREATE_IMG') {
+          const imgArr = message.imgArr || [];
+          // 路径前缀
+          const prefixPath = this.selectedFilePath.substring(0, this.selectedFilePath.lastIndexOf('/'));
+
+          // 文件名
+          const fileName = this.selectedFilePath.substring(
+            this.selectedFilePath.lastIndexOf('/') + 1,
+            this.selectedFilePath.length
+          );
+
+          // 遍历base64数组，循环生成webp图片
+          imgArr.forEach((imgBase64, idx) => {
+            const hashStr = shorthash.unique(getNonce());
+            const shortHashStr = hashStr.length > 4 ? hashStr.substring(0, 4) : hashStr;
+            const base64 = imgBase64.replace(/^data:image\/\w+;base64,/, ''); // 去掉图片base64码前面部分data:image/png;base64
+            const dataBuffer = new Buffer(base64, 'base64'); // 把base64码转成buffer对象
+            const webpFileName = `${fileName}.fuckdoc.${shortHashStr}.webp`;
+
+            fs.writeFile(`${prefixPath}/${webpFileName}`, dataBuffer, e => {
+              if (e) {
+                WebviewPanel.currentPanel.webview.postMessage({
+                  type: 'CREATE_IMG_ERROR',
+                  imgName: webpFileName,
+                  index: idx,
+                });
+              } else {
+                WebviewPanel.currentPanel.webview.postMessage({
+                  type: 'CREATE_IMG_DONE',
+                  imgName: webpFileName,
+                  index: idx,
+                });
+              }
+            });
+          });
         }
-      )
+      },
+      undefined,
+      this._disposables
+    );
+  }
 
-      panel.webview.onDidReceiveMessage(
-        (message) => {
-          const base64 = message.base64.replace(/^data:image\/\w+;base64,/, '') //去掉图片base64码前面部分data:image/png;base64
-          const dataBuffer = new Buffer(base64, 'base64') //把base64码转成buffer对象，
+  _dispose() {
+    WebviewPanel.currentPanel.dispose();
+    WebviewPanel.currentPanel = undefined;
 
-          sharp(dataBuffer).toFile(
-            `${prefixPath}/test.webp`,
-            (err: any, info: any) => {
-              console.log('done')
-            }
-          )
-          console.log('dataBuffer:::', dataBuffer)
-        },
-        undefined,
-        context.subscriptions
-      )
+    while (this._disposables.length) {
+      const x = this._disposables.pop();
+      if (x) {
+        x.dispose();
+      }
+    }
+  }
 
-      const scriptPath = vscode.Uri.joinPath(
-        context.extensionUri,
-        'media',
-        'main.js'
-      )
-      // And the uri we use to load this script in the webview
-      const scriptUri = panel.webview.asWebviewUri(scriptPath)
+  _createPanel() {
+    // 创建一个新的panel
+    return vscode.window.createWebviewPanel('addPic', '添加图片', vscode.ViewColumn.One, {
+      enableScripts: true,
+      localResourceRoots: [vscode.Uri.joinPath(this.extensionUri, 'media')],
+    });
+  }
 
-      panel.webview.html = `
+  _update() {
+    console.log('update');
+
+    WebviewPanel.currentPanel.webview.html = this._createHtml();
+  }
+
+  _createHtml() {
+    // js文件
+    const scriptPath = vscode.Uri.joinPath(this.extensionUri, 'media', 'main.js');
+    const scriptUri = WebviewPanel.currentPanel.webview.asWebviewUri(scriptPath);
+
+    // html模板
+    return `
       <!DOCTYPE html>
       <html lang="en">
         <head>
@@ -58,49 +128,72 @@ export function activate(context: vscode.ExtensionContext) {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title></title>
           <link rel="icon" href="favicon.ico" type="image/x-icon" />
-          <script defer src="${scriptUri}"></script>
         </head>
         <body>
           <div id="root"></div>
+          <script>
+            window._fuckdoc_current_component_path = '${this.selectedFilePath}';
+
+            if (!window._vscode) {
+              window._vscode = acquireVsCodeApi();
+            }
+
+            ${this._isDiff ? 'window._vscode.setState([])' : ''}
+          </script>
+          <script src="${scriptUri}"></script>
         </body>
       </html>
-
-      `
-
-      //   panel.webview.html = `
-      // <textarea id="result"></textarea>
-      // <img id="img-preview"/>
-      // <script>
-      //   document.getElementById('result').addEventListener('paste', function(e) {
-      //     // 阻止粘贴
-      //     e.stopPropagation();
-      //     e.preventDefault();
-      //     // 获取剪贴板信息
-      //     var clipboardData = e.clipboardData || window.clipboardData;
-      //     var items = clipboardData.items;
-      //     for(var i=0; i<items.length; i++) {
-      //       var item = items[i];
-      //       if (item.kind == 'file') {
-      //         var pasteFile = item.getAsFile();
-      //         var reader = new FileReader();
-      //         reader.onload = function(event) {
-      //           // 将结果显示在<textarea>中
-      //           document.getElementById('result').value = event.target.result;
-      //           document.getElementById('img-preview').src = event.target.result;
-
-      //           acquireVsCodeApi().postMessage({base64: event.target.result});
-      //         }
-      //         // 将文件读取为BASE64格式字符串
-      //         reader.readAsDataURL(pasteFile);
-      //         break;
-      //       }
-      //     }
-      //   })
-      // </script>
-      // `
-    })
-  )
+    `;
+  }
 }
 
-// this method is called when your extension is deactivated
+function injectCode(title: string) {
+  const editor = vscode.window.activeTextEditor;
+  const cursorPosition = editor.selection.active; // a vscode.Position
+
+  console.log('cursorPosition:::', cursorPosition);
+  vscode.window.activeTextEditor.edit(builder => {
+    builder.replace(
+      cursorPosition,
+      `/**
+* ${title}
+* 
+* @title 这是标题
+* @description 这是一段测试描述
+* @param {string} arg1 这是参数1
+* @param {number} arg2 这是参数2
+*/`
+    );
+  });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fuckdoc.addPic', async (uri: any) => {
+      const selectedFilePath = uri.path as string;
+
+      try {
+        const isDiff = globalSelectedFilePath && globalSelectedFilePath !== selectedFilePath;
+        new WebviewPanel(selectedFilePath, context.extensionUri, isDiff);
+
+        globalSelectedFilePath = selectedFilePath;
+      } catch (e) {
+        console.log('e:::', e);
+      }
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fuckdoc.addAnnotationFC', async (uri: any) => {
+      injectCode('F:C');
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fuckdoc.addAnnotationFF', async (uri: any) => {
+      injectCode('F:F');
+    })
+  );
+}
+
 export function deactivate() {}
